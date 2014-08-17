08f05e9b44382ba80241fb46982d716017b978d0
sue445 <sue445@sue445.net>
2014-08-18 00:48:38 +0900

    be  padrino g controller projects get:index

---
+++ app/helpers/projects_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Gemoire
+  class App
+    module ProjectsHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers ProjectsHelper
+  end
+end
---
+++ spec/app/helpers/projects_helper_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe "Gemoire::App::ProjectsHelper" do
+  pending "add some examples to (or delete) #{__FILE__}" do
+    let(:helpers){ Class.new }
+    before { helpers.extend Gemoire::App::ProjectsHelper }
+    subject { helpers }
+
+    it "should return nil" do
+      expect(subject.foo).to be_nil
+    end
+  end
+end
