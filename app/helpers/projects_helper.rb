012d97777e42e5cbbe5fc9cbbdd0a9d862b97996
sue445 <sue445@sue445.net>
2014-09-11 00:18:12 +0900

    Show short sha1

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,6 +6,10 @@ module Gemoire
       def project_doc_path(project)
         File.join(Global.gemoire.doc_path, project.name, "index.html")
       end
+
+      def short_sha1(project)
+        project.commit[0, 7] if project.commit.present?
+      end
     end
 
     helpers ProjectsHelper
