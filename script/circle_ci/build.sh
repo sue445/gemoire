1119b5f3ed1e0fe7c77924d2f9b30c211bfb3266
sue445 <sue445@sue445.net>
2014-09-14 23:43:44 +0900

    CI test : red -> green

--- script/circle_ci/build.sh
+++ script/circle_ci/build.sh
@@ -6,6 +6,7 @@
 # DATABASE=postgresql ./script/circle_ci/build.sh
 
 cp ./config/global/gemoire.yml{.example,}
+cp ./config/global/redis.yml{.example,}
 cp ./script/circle_ci/database.yml.$DATABASE config/database.yml
 RACK_ENV=test bundle exec rake ar:migrate:reset
 bundle exec rspec --tag ~skip_on_ci
