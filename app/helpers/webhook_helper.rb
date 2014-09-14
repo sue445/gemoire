61d3c443e17a5df9cdffea3f6db1d89399a0ee19
sue445 <sue445@sue445.net>
2014-09-15 01:17:59 +0900

    padrino g controller Webhook post:index post:github post:bitbucket post:gitlab

---
+++ app/helpers/webhook_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Gemoire
+  class App
+    module WebhookHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers WebhookHelper
+  end
+end
