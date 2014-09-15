4f213ca5456165b372f1270b48f918fe157b0304
sue445 <sue445@sue445.net>
2014-09-15 18:22:16 +0900

    Impl bitbucket

--- app/controllers/webhook.rb
+++ app/controllers/webhook.rb
@@ -21,9 +21,10 @@ Gemoire::App.controllers :webhook do
   # end
 
   post :index do
-    update_projects(@payload[:remote_url], @payload[:branch])
+    update_projects(remote_url: @payload[:remote_url], branch: @payload[:branch])
   end
 
+  # cf) https://developer.github.com/v3/activity/events/types/#pushevent
   post :github do
     remote_urls = [
       @payload[:repository][:git_url],
@@ -32,34 +33,54 @@ Gemoire::App.controllers :webhook do
     ]
     branch = @payload[:ref].gsub("refs/heads/", "")
 
-    update_projects(remote_urls, branch)
+    update_projects(remote_url: remote_urls, branch: branch)
   end
 
+  # cf) https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
   post :bitbucket do
-
+    repository_url = @payload[:canon_url] + @payload[:repository][:absolute_url].gsub(%r(/$), "")
+    branches = @payload[:commits].map{|commit| commit[:branch] }
+    update_projects(repository_url: repository_url, branch: branches)
   end
 
+  # cf) https://github.com/gitlabhq/gitlabhq/blob/master/doc/web_hooks/web_hooks.md
   post :gitlab do
 
   end
 
-  before do
-    begin
+  before except: [:bitbucket] do
+    with_json_parser_error_handling do
       @payload = JSON.parse(request.body.read).with_indifferent_access
-    rescue JSON::ParserError
-      # Bad Request
-      halt 400
+    end
+  end
+
+  before :bitbucket do
+    with_json_parser_error_handling do
+      request_body = request.body.read
+      payload_string = CGI.unescape(request_body).gsub(/^payload=/, "")
+      @payload = JSON.parse(payload_string).with_indifferent_access
     end
   end
 
   helpers do
-    def update_projects(remote_urls, branch)
-      @projects = Project.where(remote_url: remote_urls, branch: branch)
+    def update_projects(remote_url: nil, repository_url: nil, branch: nil)
+      @projects = Project.all
+      @projects.where!(remote_url: remote_url) if remote_url
+      @projects.where!(repository_url: repository_url) if repository_url
+      @projects.where!(branch: branch) if branch
+
       halt 404 if @projects.empty?
 
       @projects.map(&:update_doc_async)
 
       @projects.to_json
+    end
+
+    def with_json_parser_error_handling
+      yield
+    rescue JSON::ParserError
+      # Bad Request
+      halt 400
     end
   end
 
--- spec/app/controllers/webhook_controller_spec.rb
+++ spec/app/controllers/webhook_controller_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe "WebhookController" do
   shared_examples :a_webhook do
     context "when project is found" do
-      let(:remote_url){ valid_remote_url }
-      let(:branch)    { valid_branch }
+      let(:remote_url)    { valid_remote_url }
+      let(:repository_url){ valid_repository_url}
+      let(:branch)        { valid_branch }
 
       it{ expect(last_response).to be_ok }
       it{ expect(last_response.body).to be_json }
@@ -10,8 +11,9 @@ RSpec.describe "WebhookController" do
     end
 
     context "when project is not found" do
-      let(:remote_url){ "git@github.com:dummy/invalid.git" }
-      let(:branch)    { "master" }
+      let(:remote_url)    { "git@github.com:dummy/dummy.git" }
+      let(:repository_url){ "https://github.com/dummy/dummy" }
+      let(:branch)        { "master" }
 
       it{ expect(last_response).to be_not_found }
     end
@@ -31,8 +33,9 @@ RSpec.describe "WebhookController" do
     let(:payload){ { remote_url: remote_url, branch: branch }.to_json }
 
     it_behaves_like :a_webhook do
-      let(:valid_remote_url){ project.remote_url }
-      let(:valid_branch)    { project.branch }
+      let(:valid_remote_url)    { project.remote_url }
+      let(:valid_repository_url){ project.repository_url }
+      let(:valid_branch)        { project.branch }
     end
   end
 
@@ -46,8 +49,26 @@ RSpec.describe "WebhookController" do
     let(:payload)   { fixture("payloads/github.json") }
 
     it_behaves_like :a_webhook do
-      let(:valid_remote_url){ "git@github.com:baxterthehacker/public-repo.git" }
-      let(:valid_branch)    { "gh-pages" }
+      let(:valid_remote_url)    { "git@github.com:baxterthehacker/public-repo.git" }
+      let(:valid_repository_url){ "https://github.com/baxterthehacker/public-repo" }
+      let(:valid_branch)        { "gh-pages" }
+    end
+  end
+
+  describe "POST /webhook/bitbucket" do
+    let!(:project){ create(:project, remote_url: remote_url, branch: branch, repository_url: repository_url) }
+
+    subject!{ post "/webhook/bitbucket", payload }
+
+    let(:repository_url){ "https://bitbucket.org/sue445/hook_example" }
+    let(:remote_url)    { "https://sue445@bitbucket.org/sue445/hook_example.git" }
+    let(:branch)        { "master" }
+    let(:payload)       { fixture("payloads/bitbucket.js") }
+
+    it_behaves_like :a_webhook do
+      let(:valid_remote_url)    { "https://sue445@bitbucket.org/sue445/hook_example.git" }
+      let(:valid_branch)        { "master" }
+      let(:valid_repository_url){ "https://bitbucket.org/sue445/hook_example" }
     end
   end
 end
