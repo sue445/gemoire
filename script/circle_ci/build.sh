d071022b9efdd76267f5175cae13ec459a3ef089
sue445 <sue445@sue445.net>
2014-10-23 00:58:33 +0900

    Setup paraduct

--- script/circle_ci/build.sh
+++ script/circle_ci/build.sh
@@ -5,6 +5,7 @@
 # DATABASE=mysql ./script/circle_ci/build.sh
 # DATABASE=postgresql ./script/circle_ci/build.sh
 
+bundle install --path vendor/bundle/
 cp ./config/global/gemoire.yml{.example,}
 cp ./config/global/redis.yml{.example,}
 cp ./script/circle_ci/database.yml.$DATABASE config/database.yml
